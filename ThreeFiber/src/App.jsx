826a93cb17a63381fe0e0080cd828880de38a49e
Day-Ham <daehankimm17@gmail.com>
2025-11-08 18:59:58 +0800

    Added a Sphere

--- ThreeFiber/src/App.jsx
+++ ThreeFiber/src/App.jsx
@@ -1,13 +1,17 @@
 import {Canvas} from '@react-three/fiber'
+import { useRef } from 'react'
+function AnimatedBox(){
+  const boxRef= useRef();
+}
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div id="canvas-container">
-      <Canvas>
-        {
-          
-        }
+    <div id="canvas-container" style ={{width:'100vw', height:'100vh'}}>
+      <Canvas style={{ background: 'white' }}>
+        <mesh>
+          <sphereGeometry args={[3,64,64]}/>
+          <meshToonMaterial color={0xff0000} />
+          <directionalLight position={[2,5,0]}/>
+        </mesh>
       </Canvas>
 
     </div>
